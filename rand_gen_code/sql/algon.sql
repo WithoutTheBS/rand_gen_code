1896e76d00476b0b89dab8bd5a327e9ab364cac8
wtbs <anthonyowork@gmail.com>
2023-12-18 00:23:46 -0700

    finally
    I think this is actually it

--- rand_gen_code/sql/algon.sql
+++ rand_gen_code/sql/algon.sql
@@ -1,22 +1,40 @@
--- Truncate tables based on filenames
-DECLARE @TableName NVARCHAR(MAX);
+EXEC sp_configure 'show advanced options', 1;
+RECONFIGURE;
+EXEC sp_configure 'xp_cmdshell', 1;
+RECONFIGURE;
+
+DECLARE @CmdOutput TABLE (Line NVARCHAR(MAX));
+
+INSERT INTO @CmdOutput
+EXEC xp_cmdshell 'dir /b "C:\MyFolder\algon.sql"';
+
+DECLARE @DirPath NVARCHAR(MAX);
+
+SELECT TOP 1 @DirPath = LEFT(Line, CHARINDEX('\', Line, CHARINDEX('\', Line) + 1) - 1)
+FROM @CmdOutput
+WHERE Line LIKE '%algon.cmd';
+
 DECLARE @Sql NVARCHAR(MAX);
 
-DECLARE cur CURSOR FOR
-SELECT FileName FROM FileList;
+SET @Sql = 'CREATE TABLE FileList (FileName NVARCHAR(MAX));' +
+           'BULK INSERT FileList FROM ''' + @DirPath + '\FileList.txt''' +
+           ' WITH (FIELDTERMINATOR = '' '', ROWTERMINATOR = ''\n'');' +
+           'DECLARE @TableName NVARCHAR(MAX);' +
+           'WHILE EXISTS (SELECT * FROM FileList)' +
+           'BEGIN' +
+           '    SELECT TOP 1 @TableName = FileName FROM FileList;' +
+           '    IF OBJECTPROPERTY(OBJECT_ID(@TableName), ''IsTable'') = 1' +
+           '    BEGIN' +
+           '        SET @Sql = ''TRUNCATE TABLE '' + QUOTENAME(@TableName);' +
+           '        EXEC sp_executesql @Sql;' +
+           '    END' +
+           '    DELETE FROM FileList WHERE FileName = @TableName;' +
+           'END' +
+           'DROP TABLE FileList;';
 
-OPEN cur;
-FETCH NEXT FROM cur INTO @TableName;
+EXEC sp_configure 'xp_cmdshell', 0;
+RECONFIGURE;
+EXEC sp_configure 'show advanced options', 0;
+RECONFIGURE;
 
-WHILE @@FETCH_STATUS = 0
-BEGIN
-    IF OBJECTPROPERTY(OBJECT_ID(@TableName), 'IsTable') = 1
-    BEGIN
-        SET @Sql = 'TRUNCATE TABLE ' + QUOTENAME(@TableName);
-        EXEC sp_executesql @Sql;
-    END
-    FETCH NEXT FROM cur INTO @TableName;
-END
-
-CLOSE cur;
-DEALLOCATE cur;
+EXEC sp_executesql @Sql;
